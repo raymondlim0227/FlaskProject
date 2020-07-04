88611eb01825b1c4cf813379d4614a321536938e
lim chuen yang <chuenyang.lim@gdc-tech.com>
2020-07-04 13:29:13 +0800

    .: create a db

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'KILL-THIS-LOVE'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    # to disable a feature of Flask-SQLAlchemy 
+    # that I do not need, which is to signal the 
+    # application every time a change is about 
+    # to be made in the database.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
