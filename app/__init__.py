fc64edf599a9eec77f3c0c0028c73a2f155dc597
lim chuen yang <chuenyang.lim@gdc-tech.com>
2020-07-04 22:59:16 +0800

    .; able to register new user, login and logout

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # app = Flask(__name__)
 # "app" mean app directory
@@ -9,5 +10,7 @@ app = Flask("app")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login' # 'login' mean the name would use url_for() call to get the url
 
 from app import routes, models
