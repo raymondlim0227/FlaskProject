7bdd994653953d5576d0cd18166756c7011fa10d
lim chuen yang <chuenyang.lim@gdc-tech.com>
2020-07-04 10:45:44 +0800

    .: create login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 # app = Flask(__name__)
 # "app" mean app directory
 app = Flask("app")
+app.config.from_object(Config)
 
 from app import routes
