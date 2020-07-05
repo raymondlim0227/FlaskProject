61616986599e2a6f4225c020354f52eac0c8df75
lim chuen yang <chuenyang.lim@gdc-tech.com>
2020-07-05 15:28:29 +0800

    .:edit profile

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from app import app, db
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm, RegistrationForm
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from datetime import datetime
+from werkzeug.urls import url_parse
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -34,7 +42,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(url_for(next_page))
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
@@ -68,3 +76,18 @@ def user(username):
     ]
     return render_template('user.html', user=user, posts=posts)
 
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
