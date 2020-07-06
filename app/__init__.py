c6d8bd0cbf5887fe16f211e7fc2e3833ea71ece1
lim chuen yang <chuenyang.lim@gdc-tech.com>
2020-07-06 21:39:26 +0800

    .:add log .: display warning if user edit the profile and input the username that is existing in db

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 # app = Flask(__name__)
 # "app" mean app directory
 app = Flask("app")
@@ -13,4 +17,17 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' # 'login' mean the name would use url_for() call to get the url
 
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 from app import routes, models, errors
